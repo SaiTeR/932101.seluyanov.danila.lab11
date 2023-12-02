025fa6add1719a411a26648a4ae4dc38da8c7be2
Danila Seluyanov <dan.seluyanov@gmail.com>
2023-12-03 04:14:50 +0700

    FINALLY I DID IT!

---
+++ Models/CalcModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab11.Models
+{
+    public class CalcModel
+    {
+        public int A;
+        public int B;
+
+        public int Sum;
+        public int Difference;
+        public int Product;
+        public double Quotient;
+    }
+}
---
+++ Services/CalculationService.cs
@@ -0,0 +1,58 @@
+﻿using Lab11.Models;
+
+public interface ICalculationService
+{
+    int Add(int a, int b);
+    int Subtract(int a, int b);
+    int Multiply(int a, int b);
+    double Divide(int a, int b);
+
+    public CalcModel createCalcModel()
+    {
+        Random rnd = new Random();
+        int a = rnd.Next(0, 11);
+        int b = rnd.Next(0, 11);
+
+        var Calc = new CalcModel
+        {
+            Sum = Add(a, b),
+            Difference = Subtract(a, b),
+            Product = Multiply(a, b),
+            Quotient = Divide(a, b)
+        };
+
+        return Calc;
+    }
+}
+
+public class CalculationService : ICalculationService
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Деление на 0!!!");
+        }
+        return (double)a / b;
+    }
+
+    
+}
+
+
+
